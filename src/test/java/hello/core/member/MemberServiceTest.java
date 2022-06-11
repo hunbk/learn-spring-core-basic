4c7a6029509c4f6bf299fd3c00da9df7fbfe7303
hunbk <shyun823@gmail.com>
2022-06-11 20:13:13 +0900

    Test: MemberService 테스트

---
+++ src/test/java/hello/core/member/MemberServiceTest.java
@@ -0,0 +1,26 @@
+package hello.core.member;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MemberServiceTest {
+
+    MemberService memberService = new MemberServiceImpl();
+
+    @Test
+    void join() {
+        //given
+        Member member = new Member(1L, "member1", Grade.BASIC);
+
+        //when
+        memberService.join(member);
+        Member findMember = memberService.findMember(1L);
+
+        //then
+        assertEquals(member, findMember);
+
+    }
+
+}
